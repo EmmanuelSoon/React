259a1407d7cff9366ebd20b6a4944365cc6d81f0
Emmanuel Soon <reach.emmanuelsoon@gmail.com>
2022-04-16 22:20:39 +0800

    presentational and container components

--- src/Components/MenuComponent.js
+++ src/Components/MenuComponent.js
@@ -1,27 +1,23 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody,
   CardTitle } from 'reactstrap';
-import Dishdetail from './dishdetailComponent';
+
 
 class Menu extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-          selectedDish: null
-        };
+
     }
 
-    onDishSelect(dish) {
-      this.setState({ selectedDish: dish});
-  }
 
 
-  render() {
+
+  render() { //render displays whatever is in the return code.
     const menu = this.props.dishes.map((dish) => {
         return (
           <div  className="col-12 col-md-5 m-1">
             <Card key={dish.id}
-              onClick={() => this.onDishSelect(dish)}>
+              onClick={() => this.props.onClick(dish.id)}>
               <CardImg width="100%" src={dish.image} alt={dish.name} />
               <CardImgOverlay>
                   <CardTitle>{dish.name}</CardTitle>
@@ -31,17 +27,15 @@ class Menu extends Component {
         );
     });
 
-    return (
+    return (//render the dishdetais using the <Dishdetails> 
         <div className="container">
             <div className="row">
                 {menu}
             </div> 
-                       
-            <Dishdetail dish = {this.state.selectedDish} />
-        </div>
+          </div>
         
     );
     }
 }
 
-export default Menu;
+export default Menu; //always need to export the component 
