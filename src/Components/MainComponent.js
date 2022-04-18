2e54d53514265f9249ec00aafb2afae628a14f35
Emmanuel Soon <reach.emmanuelsoon@gmail.com>
2022-04-18 17:26:16 +0800

    Assignment 2 answers

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
-import Home from '.HomeComponent';
+import Home from './HomeComponent';
 import Menu from './MenuComponent'
 import Contact from './ContactComponent';
 import DishDetail from './dishdetailComponent';
+import About from './AboutComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
@@ -37,14 +39,29 @@ class Main extends Component {
         />
       );
     }
+
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0] } 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+
+    const AboutUs = () => {
+      return (
+        <About leaders = {this.state.leaders}/>
+      )
+    }
     
     return ( //any function that does not match home or menu will be redirected to home 
       <div>
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
-          <Route exact path='/contact' component={Contact}/> 
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
+          <Route path="/menu/:dishId" component={DishWithId} />
+          <Route exact path='/contact' component={Contact}/> 
+          <Route exact path="/aboutus" component={AboutUs}/>
           <Redirect to="/home" /> 
         </Switch>
         <Footer/>
--- src/Components/dishdetailComponent.js
+++ src/Components/dishdetailComponent.js
@@ -63,35 +63,39 @@ import {Link} from 'react-router-dom'
 
 
     const DishDetail = (props) => {
-            
 
-            if(props.dish == null){
-                return <div></div>
-            }
-
+        if (props.dish != null) {
             return (
-                <div className='container'>
-                    <div className='row'>
+                <div className="container">
+                    <div className="row">
                         <Breadcrumb>
-                            <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
-                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                            <BreadcrumbItem>
+                                <Link to="/menu">Menu</Link>
+                            </BreadcrumbItem>
+                            <BreadcrumbItem active>
+                                { props.dish.name }
+                            </BreadcrumbItem>
                         </Breadcrumb>
-                        <div className='col-12'>
-                        <h3>{props.dish.name}</h3>
-                        <hr />
+    
+                        <div className="col-12">
+                            <h3> {props.dish.name}</h3>
+                            <hr />
+                        </div>
                     </div>
-                </div>
+    
                     <div className='row'>
-                        <RenderDish dish={props.dish}/>
-                        <RenderComments comments={props.comments}/>
+                        <RenderDish dish={ props.dish } />
+                        <RenderComments comments={ props.comments } />
                     </div>
                 </div>
-
             )
+        }
 
+        else {
+            return (<div></div>);
+        }
 
-        
     }
 
   
-export default DishDetail
+export default DishDetail;
