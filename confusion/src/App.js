189f12bca47aadd260f3901027380ef835662d73
Emmanuel Soon <reach.emmanuelsoon@gmail.com>
2022-05-12 22:47:00 +0800

    minor edits

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
 import Main from './Components/MainComponent'
-import {browserRouter} from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
