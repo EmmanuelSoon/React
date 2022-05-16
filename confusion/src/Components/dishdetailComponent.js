27bb16ad5a0f6aa6e918a01e1a61b36818312bd3
Emmanuel Soon <reach.emmanuelsoon@gmail.com>
2022-05-16 18:03:03 +0800

    Fetch from Server

--- confusion/src/Components/dishdetailComponent.js
+++ confusion/src/Components/dishdetailComponent.js
@@ -7,6 +7,7 @@ import {Link} from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 
 
@@ -128,7 +129,7 @@ class CommentForm extends Component {
             return(
                 <div className='col-12 col-md-5 m-1'>
                     <Card>
-                        <CardImg top src={dish.image} alt={dish.name} />
+                        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                         <CardBody>
                             <CardTitle>{dish.name}</CardTitle>
                             <CardText>{dish.description}</CardText>
--- confusion/src/redux/ActionTypes.js
+++ confusion/src/redux/ActionTypes.js
@@ -2,10 +2,13 @@ export const ADD_COMMENT = 'ADD_COMMENT';
 export const ADD_DISHES = 'ADD_DISHES';
 export const DISHES_LOADING = 'DISHES_LOADING';
 export const DISHES_FAILED = 'DISHES_FAILED';
+export const ADD_COMMENTS = 'ADD_COMMENTS';
+export const COMMENTS_FAILED = 'COMMENTS_FAILED';
+export const PROMOS_LOADING = 'PROMOS_LOADING';
+export const ADD_PROMOS = 'ADD_PROMOS';
+export const PROMOS_FAILED = 'PROMOS_FAILED';
 
 
 
 
-
-export const ADD_LEADER = 'ADD_LEADER';
-export const ADD_PROMO = 'ADD_PROMO';
+export const ADD_LEADERS = 'ADD_LEADERS';
