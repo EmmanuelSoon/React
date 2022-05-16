880747a81fa56e514c41b6bfbe0f567529117a9a
Emmanuel Soon <reach.emmanuelsoon@gmail.com>
2022-05-16 18:44:46 +0800

    Fetch post comment

--- confusion/src/Components/dishdetailComponent.js
+++ confusion/src/Components/dishdetailComponent.js
@@ -38,7 +38,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleCommentModal();
-        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render(){
@@ -117,13 +117,6 @@ class CommentForm extends Component {
 
 
 
-
-
-
-
-
-
-
     function RenderDish({dish}) {
         if (dish != null) 
             return(
@@ -144,7 +137,7 @@ class CommentForm extends Component {
             );
     }
 
-    function RenderComments({comments, addComment, dishId}) {
+    function RenderComments({comments, postComment, dishId}) {
         if (comments != null){
             const comt = comments.map(comment => {
                 return (
@@ -168,7 +161,7 @@ class CommentForm extends Component {
                 <ul className='list-unstyled'>
                     {comt}
                 </ul>  
-                <CommentForm dishId={dishId} addComment={addComment}/>               
+                <CommentForm dishId={dishId} postComment={postComment}/>               
             </div>
             )
         }
@@ -221,7 +214,7 @@ class CommentForm extends Component {
                     <div className='row'>
                         <RenderDish dish={ props.dish } />
                         <RenderComments comments={ props.comments } 
-                            addComment= {props.addComment}
+                            postComment= {props.postComment}
                             dishId = { props.dish.id }
                             />
                     </div>
