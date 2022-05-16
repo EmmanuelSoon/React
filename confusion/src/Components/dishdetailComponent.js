7b8ff2b5cac93746f50cd6c2231cd29184d43c8c
Emmanuel Soon <reach.emmanuelsoon@gmail.com>
2022-05-16 12:34:03 +0800

    Redux thunk

--- confusion/src/Components/dishdetailComponent.js
+++ confusion/src/Components/dishdetailComponent.js
@@ -6,6 +6,8 @@ import { Card, CardImg, CardText, CardBody,
 import {Link} from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
+import { Loading } from './LoadingComponent';
+
 
 
 const required = (val) => val && val.length;
@@ -177,8 +179,26 @@ class CommentForm extends Component {
 
 
     const DishDetail = (props) => {
+        if (props.isLoading){
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.errMess){
+            return (
+                <div className="container">
+                    <div className="row">            
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
 
-        if (props.dish != null) {
+        else if (props.dish != null) {
             return (
                 <div className="container">
                     <div className="row">
--- confusion/src/redux/ActionTypes.js
+++ confusion/src/redux/ActionTypes.js
@@ -1,4 +1,11 @@
 export const ADD_COMMENT = 'ADD_COMMENT';
-export const ADD_DISH = 'ADD_DISH';
+export const ADD_DISHES = 'ADD_DISHES';
+export const DISHES_LOADING = 'DISHES_LOADING';
+export const DISHES_FAILED = 'DISHES_FAILED';
+
+
+
+
+
 export const ADD_LEADER = 'ADD_LEADER';
 export const ADD_PROMO = 'ADD_PROMO';
